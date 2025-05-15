c5981ccc536e14c3c08fe612cb9af91abe73ee8a
Murilex <murilo.bertalha@gmail.com>
2025-05-15 20:20:49 -0300

    arvores

---
+++ src/Arvore.java
@@ -0,0 +1,9 @@
+public class Arvore {
+    public No raiz;
+    public No filho;
+
+    public Arvore(No raiz, No filho) {
+        this.raiz = raiz;
+        this.filho = filho;
+    }
+}
---
+++ src/No.java
@@ -0,0 +1,11 @@
+public class No {
+    public Integer conteudo;
+    public Integer NoEsq;
+    public Integer NoDir;
+
+    public No(Integer conteudo, Integer noEsq, Integer noDir) {
+        this.conteudo = conteudo;
+        NoEsq = noEsq;
+        NoDir = noDir;
+    }
+}
